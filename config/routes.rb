e0b1b1c8e3900ddd2a2a45bf0ff22f6f0d6717a9
CharlesWarsh <charles.warsh@brickfish.com>
2016-03-10 12:41:02 -0600

    devise seems to be installed yessss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: { sessions: "users/sessions" }
   root to: 'intranets#homepage'
 
   # The priority is based upon order of creation: first created -> highest priority.
