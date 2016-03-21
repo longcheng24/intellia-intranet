4078e5e8b79b5c5efae1a11860cb6fce4c59cdbc
long cheng <longcheng0229@gmail.com>
2016-03-21 16:53:14 -0500

    article index show pages and paginate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   resources :feeds
-  
+  resources :articles
+
+
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
 
   devise_scope :user do
