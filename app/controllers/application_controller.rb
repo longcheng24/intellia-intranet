3a24c6a5447b61f04f8f301de7c0f4bffd7abfc1
Rich Baukovic <rich.baukovic@brickfish.com>
2016-03-24 15:42:34 -0500

    fixed user edit profile info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
 
   respond_to :html, :json
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:account_update) { |u|
+  		u.permit(:title, :first_name, :last_name, :department, :phone, :email, :photo, :password, :password_confirmation, :current_password)
+  	}
+  end
 end
