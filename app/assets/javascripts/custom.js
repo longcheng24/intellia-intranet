794358f46a86257dd04a2742c16ff9ad7251f99e
long cheng <longcheng0229@gmail.com>
2016-03-30 16:34:04 -0500

    twitter load fix

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
     $(this).addClass("activeTab")
     $(this).siblings().removeClass("activeTab")
   })
+
+    $("#twitterContainer").html('<a class="twitter-timeline" href="https://twitter.com/intelliatweets" data-widget-id="713109574643888128" data-chrome="noheader nofooter noborders transparent"></a>')
+    twttr.widgets.load();
+
 })
