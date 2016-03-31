d04a29d34b97754090c9bf737593aad87d575b77
Rich Baukovic <rich.baukovic@brickfish.com>
2016-03-31 14:42:23 -0500

    refactored method for displaying user's department in view files

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,52 +11,44 @@ class User < ActiveRecord::Base
 	validates :first_name, :last_name, presence: true
 
 	scope :human_resources, -> { where department: HUMANRESOURCES }
-	scope :finance, -> { where department: FINANCE }
-	scope :research_development, -> { where department: RESEARCH }
+  scope :finance, -> { where department: FINANCE }
+  scope :research_development, -> { where department: RESEARCH }
 
-  HUMANRESOURCES	= 10
-  FINANCE					= 20
-  RESEARCH				= 30
+  HUMANRESOURCES  = 10
+  FINANCE         = 20
+  RESEARCH        = 30
 
   DEPARTMENT_NAMES = ['Human Resources', 'Finance', 'R&D']
   DEPARTMENT_VALUES = [HUMANRESOURCES, FINANCE, RESEARCH]
 
+  DISPLAY_NAMES = {HUMANRESOURCES => 'Human Resources', FINANCE => 'Finance', RESEARCH => 'Research & Development'}.freeze
+
   def self.departments
-  	DEPARTMENT_NAMES.map.with_index { | name, i | [ name, DEPARTMENT_VALUES[i] ] }
+    DEPARTMENT_NAMES.map.with_index { | name, i | [ name, DEPARTMENT_VALUES[i] ] }
   end
 
   def self.department_name department_value
-  	DEPARTMENT_NAMES[DEPARTMENT_VALUES.index department_value]
+    DEPARTMENT_NAMES[DEPARTMENT_VALUES.index department_value]
   end
 
   def self.department_value department_name
-  	DEPARTMENT_VALUES[DEPARTMENT_NAMES.index department_name]
+    DEPARTMENT_VALUES[DEPARTMENT_NAMES.index department_name]
   end
 
   def human_resources?
-  	department == HUMANRESOURCES
+    department == HUMANRESOURCES
   end
 
   def finance?
-  	department == FINANCE
+    department == FINANCE
   end
 
   def research_development?
-  	department == RESEARCH
+    department == RESEARCH
   end
 
   def user_dept
-    if self.department == HUMANRESOURCES
-      return 'Human Resources'
-    elsif self.department == FINANCE
-      return 'Finance'
-    else
-      return 'Research & Development'
-    end
+    DISPLAY_NAMES[self.department] || 'Research & Development'
   end
-
-  # def all?
-  # 	department == 'All'
-  # end
 
 end
