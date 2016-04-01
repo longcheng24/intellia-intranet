53ab8edeb11071e9f85cb06631013ed96cf10cba
Rich Baukovic <rich.baukovic@brickfish.com>
2016-04-01 15:58:24 -0500

    updated config file to use .env variable for sendgrid username

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'brickfish_developer',
+  :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
