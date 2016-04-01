2d0cc32343b6701c350f88f6b6176df4354ea683
Rich Baukovic <rich.baukovic@brickfish.com>
2016-04-01 16:14:06 -0500

    more config changes for mail send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'intellia-intranet.herokuapp.com' } # For devise
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
     :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
