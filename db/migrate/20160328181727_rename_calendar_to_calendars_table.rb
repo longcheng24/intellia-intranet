f4c845eb51994aacdb84e865a41c168ca65bb26e
“WacoChili” <rich.baukovic@brickfish.com>
2016-04-01 12:13:41 -0500

    removed another migration related to earlier issue

--- db/migrate/20160328181727_rename_calendar_to_calendars_table.rb
+++
@@ -1,8 +0,0 @@
-class RenameCalendarToCalendarsTable < ActiveRecord::Migration
-  def up
-    rename_table :calendar, :calendars
-  end
-  def down
-    rename_table :calendars, :calendar
-  end
-end
